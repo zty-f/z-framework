ef1bb5fa548329cac8fe43028350c4c58b461911
张天泳 <w_zhangtianyong@xiwang.com>
2023-09-10 14:39:11 +0800

    使用net/http实现接口处理

---
+++ zweb/http-base/base1/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// 定义一个handler方法
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	for k, v := range req.Header {
+		fmt.Fprintf(w, "Header[%s] = %q\n", k, v)
+	}
+}
