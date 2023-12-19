c895e3048b995bd5cbaef4db0a36ece470635719
w_zhangtianyong <w_zhangtianyong@xiwang.com>
2023-12-19 16:55:30 +0800

    zorm-实现Find和Select方法，提供自定义sql语句

---
+++ zorm/3-save-query/zorm.go
@@ -0,0 +1,44 @@
+package zorm
+
+import (
+	"database/sql"
+	"zorm/dialect"
+	"zorm/log"
+	"zorm/session"
+)
+
+type Engine struct {
+	db      *sql.DB
+	dialect dialect.Dialect
+}
+
+func NewEngine(driver, source string) (e *Engine, err error) {
+	db, err := sql.Open(driver, source)
+	if err != nil {
+		log.Error(err)
+	}
+	// 发送ping测试是否连接成功
+	if err = db.Ping(); err != nil {
+		log.Error(err)
+		return
+	}
+	dial, ok := dialect.GetDialect(driver)
+	if !ok {
+		log.Errorf("dialect %s Not Found", driver)
+		return
+	}
+	e = &Engine{db: db, dialect: dial}
+	log.Info("connect database success")
+	return
+}
+
+func (e *Engine) Close() {
+	if err := e.db.Close(); err != nil {
+		log.Error("Failed to close database")
+	}
+	log.Info("close database success")
+}
+
+func (e *Engine) NewSession() *session.Session {
+	return session.New(e.db, e.dialect)
+}
